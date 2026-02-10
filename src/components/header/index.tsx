866bdf6d64a22699d37839bd545909d57dacb7d6
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2026-02-09 22:13:39 -0300

    feat: criando e estilizando componente header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,32 @@
+import { Link } from 'react-router';
+import logoImg from '../../assets/logo.svg';
+import { FiUser, FiLogIn } from 'react-icons/fi';
+
 export function Header() {
+  const signed = false;
+  const loadingAuth = false;
+
   return (
-    <header>
-      <h1>header da pagina</h1>
-    </header>
+    <div className="full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full items-center max-w-7xl justify-between px-4 mx-auto">
+        <Link to="/">
+          <img src={logoImg} alt="Logo do site" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900 ">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <FiLogIn size={22} color="#000" />
+          </Link>
+        )}
+      </header>
+    </div>
   );
 }
